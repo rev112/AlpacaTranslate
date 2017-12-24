73c1ac6fc5910d96fb4c0fe6bd91745cc5ae62fd
Anton Ovchinnikov <revolver112@gmail.com>
2017-12-24 23:57:06 +0300

    Init

---
+++ select.js
@@ -0,0 +1,38 @@
+console.info("Extension is loaded");
+
+var isShown = false;
+const dialogId = 'select_tooltip_dialog';
+
+function showTooltip(text) {
+  const elem = document.createElement('div');
+  elem.id = dialogId;
+  elem.innerHTML = text;
+  document.body.appendChild(elem);
+  isShown = true; 
+}
+
+function hideTooltip() {
+  const elems = document.querySelectorAll("#" + dialogId);
+  for (var i = 0; i < elems.length; i++) {
+    elems[i].remove(); 
+  }
+
+  isShown = false;
+}
+
+document.addEventListener("dblclick", (e) => {
+  const selObj = window.getSelection().toString();
+  const withShift = e.shiftKey;
+  console.info("Double click, with SHIFT: ", withShift); 
+
+  if (!withShift || selObj.length > 100) {
+    return;
+  }
+
+  console.info("Selected: ", selObj);
+
+  if (isShown) {
+    hideTooltip();
+  }
+  showTooltip(selObj);
+});
