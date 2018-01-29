1b3745ffcb3302fd3af33d3d9bb58db53ff33ef9
Anton Ovchinnikov <revolver112@gmail.com>
2018-01-29 23:41:38 +0100

    Add audio button to the tooltip (with CSP issues)

--- src/select.js
+++ src/select.js
@@ -1,25 +1,36 @@
 /* globals fetch, browser, Handlebars */
 'use strict';
 
+let enableTestData = false;
 const enableDebugOutput = true;
-const enableTestData = false;
 const dialogClass = 'transbox';
 const transClass = 'trans-size';
 const errorClass = 'err-size';
 let externalImgUrl = 'src/icons/external_link.png';
+let soundImgUrl = 'src/icons/sound.svg';
 let isShown = false;
 
 // Allow the code to work both in the context of a webextension and a normal web-page
 if (typeof browser !== 'undefined') {
   externalImgUrl = browser.extension.getURL(externalImgUrl);
+  soundImgUrl = browser.extension.getURL(soundImgUrl);
 }
 
 const tooltipTemplate = `
 <div class="trans-normalized">
-  <span>{{normalized}}</span>
+  <div class="trans-first-line">
+    <span>{{normalized}}</span>
+    <span onclick="document.getElementById('trans-audio').play(); return false">
+      <img style="height:1em" src="{{soundImg}}"/>
+    </span>
+    <span class="trans-desc">{{wordtype}}</span>
+  </div>
   <a target="_blank" href="{{url}}" title="Linguee">
     <img class="trans-link-original" src="{{externalImg}}" alt="external source"/>
   </a>
+  <audio id="trans-audio" src="{{audio_url}}">
+    No audio support
+  </audio>
 </div>
 <div class="trans-list">
   {{#each translations}}
@@ -61,27 +72,28 @@ function prepareElem(e, additionalClass){
   preparingElem.classList.add(dialogClass);
   preparingElem.classList.add(additionalClass);
   preparingElem.style.left = x + 10 + 'px';
-  preparingElem.style.top = y + 10 + 'px';  
+  preparingElem.style.top = y + 10 + 'px';
   return preparingElem;
 }
 
 function showResults(results, e) {
   log_debug('Showing result tooltip...');
   const elem = prepareElem(e, transClass);
-  const template = Handlebars.compile(usedTemplate);
+  const template = Handlebars.compile(tooltipTemplate);
   results.externalImg = externalImgUrl;
+  results.soundImg = soundImgUrl;
   elem.innerHTML = template(results);
   document.body.appendChild(elem);
   isShown = true;
 }
 
 function showTranslationError(e){
-  log_debug('Showing error tooltip...');  
-  const elem = prepareElem(e, transClass);
+  log_debug('Showing error tooltip...');
+  const elem = prepareElem(e, errorClass);
   const template = Handlebars.compile(tooltipErrorTemplate);
   elem.innerHTML = template();
   document.body.appendChild(elem);
-  isShown = true; 
+  isShown = true;
 }
 
 
@@ -89,6 +101,7 @@ function getTestData() {
   const results = {
     'original':'characters',
     'normalized':'characters',
+    'wordtype':'noun, plural',
     'translations': [
       {'transString':'символы','transDesc':{'short':'pl','title':'noun, plural'}},
       {'transString':'персонажи','transDesc':{'short':'pl','title':'noun, plural'}},
@@ -97,7 +110,8 @@ function getTestData() {
       {'transString':'героини','transDesc':{'short':'pl','title':'noun, plural'}},
       {'transString':'личности','transDesc':{'short':'pl','title':'noun, plural'}}
     ],
-    'url':'https://www.linguee.com/english-russian/search?query=characters'
+    'url':'https://www.linguee.com/english-russian/search?query=characters',
+    'audio_url': 'https://www.linguee.com/mp3/EN_US/7d/7d97481b1fe66f4b51db90da7e794d9f-101.mp3'
   };
   return results;
 }
@@ -136,13 +150,21 @@ function showTooltip(text, e) {
         };
       });
 
-      let normalized_block = exact_match_block.getElementsByClassName('line lemma_desc')[0];
-      let normalized = normalized_block.getElementsByClassName('dictLink')[0].text;
+      const normalized_block = exact_match_block.getElementsByClassName('line lemma_desc')[0];
+      const normalized = normalized_block.getElementsByClassName('dictLink')[0].text;
+      const normalized_wordtype = normalized_block.getElementsByClassName('tag_wordtype')[0].textContent;
+      const audio_element = normalized_block.getElementsByClassName('audio')[0];
+      const audio_url =
+        audio_element
+          ? 'https://www.linguee.com/mp3/' + audio_element.id + '.mp3'
+          : undefined;
       let results = {
         'original': text,
         'normalized': normalized,
+        'wordtype': normalized_wordtype,
         'translations': translations,
-        'url': url
+        'url': url,
+        'audio_url': audio_url
       };
       log_debug('Results:', JSON.stringify(results));
       showResults(results, e);
